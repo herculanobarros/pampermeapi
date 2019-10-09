a96d1408037afa196ab4e6c73e17ff4bc2729295
herculanobarros <herculano.barros@my.istec.pt>
2019-10-09 22:53:51 +0100

    revert some things

--- root/app.py
+++ root/app.py
@@ -1,21 +1,28 @@
 from flask import Flask
+import os
 from root.extensions import db
 from root.resources.Babysitter import babysitter_api
 from root.resources.User import user_api
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'XYZ')
 
-def create_app():
-    db.init_app(app)
 
-    app.register_blueprint(user_api, url_prefix='/api/v1/users')
-    app.register_blueprint(babysitter_api, url_prefix='/api/v1/babysitters')
+@app.route('/', methods=['GET'])
+def index():
+    return "Hello World"
 
-    with app.app_context():
-        db.create_all()
-
-    @app.route('/', methods=['GET'])
-    def index():
-        return 'Congratulations!'
-
-    return app
+# def create_app():
+#     db.init_app(app)
+#
+#     app.register_blueprint(user_api, url_prefix='/api/v1/users')
+#     app.register_blueprint(babysitter_api, url_prefix='/api/v1/babysitters')
+#
+#     with app.app_context():
+#         db.create_all()
+#
+#     @app.route('/', methods=['GET'])
+#     def index():
+#         return 'Congratulations!'
+#
+#     return app
